4e6cfa5129baffd5656016dd4e736b971bb18041
azheng249 <andy.zheng249@gmail.com>
2016-10-13 11:53:48 -0400

    Migrate database. Moved root path in routes.rb below the subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  root 'home#home'
-
-  namespace :blog do
-    constraints subdomain: 'blog' do
-      resources :blog
-    end
+  constraints subdomain: 'blog' do
+    get '/', to: 'blogs#index'
+    resources :blogs
   end
+  
+  root 'home#home'
 end
